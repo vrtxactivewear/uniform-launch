ad0ee040b0d557a58ecb1801f3d985033141d37d
VRTX Activewear <sales@vrtxactivewear.com.au>
2026-08-25 11:07:35 +1000

    Remove basePath for proper GitHub Pages rendering

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/uniform-launch",
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
